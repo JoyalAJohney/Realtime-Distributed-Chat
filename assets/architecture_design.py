72a7dbb1dbcf8600233bbea2d8e026da7290ad46
Joyal A Johney <joyalajohny@gmail.com>
2024-02-25 10:32:17 +0530

    Update architecture diagram

--- assets/architecture_design.py
+++ assets/architecture_design.py
@@ -6,24 +6,30 @@ from diagrams.onprem.queue import Kafka
 from diagrams.onprem.database import PostgreSQL
 from diagrams.onprem.inmemory import Redis
 from diagrams.programming.framework import React
+from diagrams.aws.ml import Sagemaker as LLMModel
 
-
-
-with Diagram(name="Distributed Chat Architecture", show=False):
+with Diagram(name="", show=False):
     frontend = React("Frontend")
-    
+
     with Cluster("Backend Servers"):
         nginx = Nginx("Nginx Server")
         go_servers = [Server(f"Go Server {i}") for i in range(1, 4)]
         nginx >> go_servers
 
-    redis_pubsub = Redis("Redis Pub/Sub")
+
     kafka = Kafka("Kafka Queue")
-    go_servers >> redis_pubsub >> kafka
+    llmodel = LLMModel("LLM Model")
+    redis_pubsub = Redis("Redis Pub/Sub")
+    
+    go_servers >> redis_pubsub
+
+    redis_pubsub >> kafka
+    redis_pubsub >> llmodel
+    llmodel >> redis_pubsub
+
 
     batch_server = Server("Batch Go Server")
     db = PostgreSQL("PostgreSQL")
     
     kafka >> batch_server >> db
-
     frontend >> nginx
